0b91eb524166194d197fe515d263fbb67ba42327
mehranarji <mehranarji@gmail.com>
2023-03-02 00:51:21 +0330

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
+        fontFamily: {
+            sans: ["Lato", "sans-serif"]
+        },
         extend: {},
     },
     plugins: [],
