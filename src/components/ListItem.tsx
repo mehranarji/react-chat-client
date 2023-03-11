98d115e5773f29c85f7fdff248a37e5ab0e57ddc
mehranarji <mehranarji@gmail.com>
2023-03-12 00:18:53 +0330

    Improve list item component

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+import { ComponentPropsWithoutRef, forwardRef, ReactNode } from "react";
+
+interface ListItemProps extends ComponentPropsWithoutRef<"div"> {
+  avatar?: ReactNode;
+  children?: ReactNode;
+  className?: string;
+}
+
+const ListItem = forwardRef<HTMLDivElement, ListItemProps>((props, ref) => {
+  const { className, children, avatar, ...rest } = props;
+
+  return (
+    <div
+      className={clsx("flex items-start gap-2 overflow-hidden", className)}
+      ref={ref}
+      {...rest}
+    >
+      {avatar && <div className="w-14">{avatar}</div>}
+      <div className="min-w-0 flex-1">{children}</div>
+    </div>
+  );
+});
+
+ListItem.displayName = "ListItem";
+
+export default ListItem;
