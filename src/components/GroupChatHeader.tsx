2061dab86f39de1a24e7dd639699712af1ffd8b6
mehranarji <mehranarji@gmail.com>
2023-04-04 23:34:51 +0330

    add some fake icons

--- src/components/GroupChatHeader.tsx
+++ src/components/GroupChatHeader.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import ChatHeader from "./ChatHeader";
 import ContactAvatar from "./ContactAvatar";
 import { GroupChat } from "../app/models/Chat";
+import { VideoCameraIcon, PhoneIcon, EllipsisHorizontalCircleIcon } from "@heroicons/react/24/outline";
 
 interface GroupChatHeaderProps {
   chat: GroupChat
@@ -21,6 +22,20 @@ const GroupChatHeader: FC<GroupChatHeaderProps> = (props) => {
       <div>
         <p className="font-medium text-lg">{chat.name}</p>
         <p className="text-neutral-400">{getStatus()}</p>
+      </div>
+
+      <div className="ml-auto flex gap-1">
+        <button className="text-neutral-400 hover:text-neutral-600 transition-colors p-3">
+          <VideoCameraIcon className="w-5 h-5" />
+        </button>
+
+        <button className="text-neutral-400 hover:text-neutral-600 transition-colors p-3">
+          <PhoneIcon className="w-5 h-5" />
+        </button>
+
+        <button className="text-neutral-400 hover:text-neutral-600 transition-colors p-3">
+          <EllipsisHorizontalCircleIcon className="w-5 h-5" />
+        </button>
       </div>
     </ChatHeader>
   );
