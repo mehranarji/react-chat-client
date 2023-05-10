88d883a4feac0cd7d08c112f7abf64f77bd6aa40
mehranarji <mehranarji@gmail.com>
2023-05-10 21:24:50 +0330

    use hash router for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ChatView from "./views/ChatView";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ChatView />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
